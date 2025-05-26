9d2c86812b6d26f2eaa2310d1569d2dded5be4e5
daridev <darideveloper@gmail.com>
2025-05-26 05:43:40 -0600

    style (EtiquetaPreview): remove logo border

--- src/components/preview/EtiquetaPreview.tsx
+++ src/components/preview/EtiquetaPreview.tsx
@@ -64,8 +64,6 @@ const EtiquetaPreview = ({ formData }: EtiquetaPreviewProps) => {
               'h-64',
               'rounded-full',
               'overflow-hidden',
-              'border-2',
-              'border-primary',
               'flex-shrink-0',
               'bg-white',
               'flex',

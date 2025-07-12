13681d2b273917a4d19111f1593f9e3049808791
daridev <darideveloper@gmail.com>
2025-07-12 16:26:09 -0600

    feat (SelloRedondo): udpate fonts

--- src/constants/designFonts.ts
+++ src/constants/designFonts.ts
@@ -21,8 +21,7 @@ export const DESIGN_FONTS: Record<ProductType, FontOption[]> = {
     { value: 'androgyne-tb', label: 'Androgyne TB', fontFamily: 'AndrogyneTB' },
   ],
   'sello-redondo': [
-    { value: 'montserrat', label: 'Montserrat', fontFamily: 'Montserrat' },
-    { value: 'roboto', label: 'Roboto', fontFamily: 'Roboto' },
-    { value: 'opensans', label: 'Open Sans', fontFamily: 'OpenSans' }
+    { value: 'LiberationMono', label: 'LiberationMono', fontFamily: 'LiberationMono' },
+    { value: 'arial', label: 'Arial', fontFamily: 'Arial' },
   ]
 };
